3a10d3b04688b280d34efb2f847ab62edfd2564d
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-04-16 19:07:14 +0200

    Added states and simple logic

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import IncomeCard from "./IcomeCard";
 
 function Container () {
+
     return (
         <div className="flex flex-col items-center gap-8 bg-[#3C3C4C] p-8 rounded-xl shadow-md shadow-gray-700">
             <h1 className="text-4xl font-semibold text-[#9DA1AA]">
                 Personal Finance Tracker
             </h1>
-            <IncomeCard />
+            <IncomeCard income={income} setIcome={setIncome} />
         </div>
     );
 }
--- src/components/IcomeCard.jsx
+++ src/components/IcomeCard.jsx
@@ -1,24 +1,49 @@
-function IncomeCard() {
+import { useState } from "react";
+
+function IncomeCard({ income, setIncome }) {
+  const [incomeInput, setIncomeInput] = useState([]);
+  const [incomeDescritionInput, setIncomeDescriptionInput] = useState("")
+
+  function addIncome () {
+        setIncome([...incomeInput, { description: incomeDescritionInput.trim(), amount: incomeInput }]);
+
+        setIncome("");
+        setIncomeDescriptionInput("")
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-4">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
             <label  className="text-[hsl(0,100%,100%)]" htmlFor="icome-description">
                 Income Description
             </label>
-            <input className="border border-[hsl(0,100%,100%)] rounded-md py-1" type="text" id="icome-description" />
+            <input 
+                className="border border-[hsl(0,100%,100%)] rounded-md py-1 placeholder:text-sm placeholder:text-[#9DA1AA] placeholder:italic pl-2 caret-[hsl(0,100%,100%)] outline-none" 
+                type="text" 
+                id="icome-description" 
+                placeholder="E.g Allowance"
+                onChange={(e) => setIncomeDescriptionInput(e.target.value)}
+                value={incomeDescritionInput}
+            />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
             <label className="text-[hsl(0,100%,100%)] rounded-md" htmlFor="amount">
                 Amount
             </label>
-            <input className="border border-[hsl(0,100%,100%)] rounded-md py-1" type="number" id="amount" />
+            <input 
+                className="border border-[hsl(0,100%,100%)] rounded-md py-1" 
+                type="number" 
+                id="amount" 
+                onChange={(e) => setIncome(e.target.value)}
+                value={income}
+            />
         </div>
       </div>
-      <button className="p-4 bg-[#B7B6AC] rounded-md font-semibold tracking-[4px] cursor-pointer">
+      <button className="border-2 border-transparent hover:border-[hsl(0,100%,100%)] hover:bg-transparent transition-all duration-300 p-2 bg-[#B7B6AC] rounded-md font-semibold tracking-[4px] cursor-pointer text-[hsl(0,100%,100%)]">
         ADD
       </button>
-      <button className="p-4 bg-[#029456] rounded-md font-semibold tracking-[4px] cursor-pointer">
+      <button className="border-2 border-transparent hover:border-[hsl(0,100%,100%)] hover:bg-transparent transition-all duration-300 p-2 bg-[#029456] rounded-md font-semibold tracking-[4px] cursor-pointer text-[hsl(0,100%,100%)]">
         NEXT
       </button>
     </div>
